b4e6c241086fc2c5ddfb080efc3ecf8e96f746e9
jstagg <jstagg@gmail.com>
2020-06-13 01:01:11 -0400

    First commit

---
+++ HelloWorld/src/firstPackage/helloWorld.java
@@ -0,0 +1,10 @@
+package firstPackage;
+
+public class helloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello World" in the terminal window.
+      System.out.println("Hello World in Java");
+   }
+}
+
+// -30-
